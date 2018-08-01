7771c3ec8eea4ef4b4e6830caefe17215951d31b
David Chang <chechiachang999@gmail.com>
2018-08-01 09:03:47 +0800

    Add http parser

---
+++ http.go
@@ -0,0 +1,23 @@
+package etherscan
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func Get(url string) string {
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	return string(contents)
+}
