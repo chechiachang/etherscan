4607c94c1b98abf42c8f49260c341d14e9b5b45f
David Chang <chechiachang999@gmail.com>
2018-08-01 09:04:34 +0800

    Add get contract api

--- contract.go
+++ contract.go
@@ -4,5 +4,6 @@ const MODULE_CONTRACT = "contract"
 
 //TODO return struct
 func GetContract(contractId string) string {
-	return contractId
+	url := getURL(MODULE_CONTRACT, "getabi", contractId)
+	return Get(url)
 }
